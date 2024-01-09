07e563d72a1e3da831fc2f6b95ecabeee126abc2
Ojoshegbe <yunusaadejo20@gmail.com>
2024-01-09 11:54:25 +0100

    second commit message

---
+++ index.js
@@ -0,0 +1,26 @@
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+const ul = document.getElementById('ul');
+const li = document.getElementsByTagName('li');
+
+function opentab(tabname){
+    for(tablink of tablinks){
+       tablink.classList.remove("active-link"); 
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+function Show(){
+    ul.style.right = '0';
+}
+
+function Hide(){
+    ul.style.right = '-200px';
+}
+
+
+
